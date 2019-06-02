c3b0ebc560416ce645b9111eed6c3f1426ce9e3e
chrisperez626 <chrisperez626@gmail.com>
2019-06-02 01:39:27 -0400

    restructured folders

---
+++ client/pop.js
@@ -0,0 +1,10 @@
+let greenButton = document.getElementById('enterButton');
+
+  // chrome.storage.sync.get('color', function(data) {
+  //   changeColor.style.backgroundColor = data.color;
+  //   changeColor.setAttribute('value', data.color);
+  // });
+
+greenButton.addEventListener("click", function(){
+    console.log("yo");
+})
